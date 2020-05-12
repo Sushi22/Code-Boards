77d2615bb478aa5f3a37201f20f453d0cce9e2f2
Sushi22 <sushmitajoshi22@gmail.com>
2020-05-12 15:50:11 +0530

    Added pagination

--- newapp/views.py
+++ newapp/views.py
@@ -6,22 +6,33 @@ from .forms import NewTopicForm,PostForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login as auth_login
 from django.db.models import Count
+from django.utils import timezone
+from django.views.generic import UpdateView,ListView
+from django.utils.decorators import method_decorator
 
 # Create your views here.
 from django.http import HttpResponse
 
-def home(request):
-    boards = Boards.objects.all()
-    
-    return render(request,'home.html',{'boards':boards})
+class BoardsListView(ListView):
+    model=Boards
+    template_name='home.html'
+    context_object_name='boards'
 
-def board_topics(request,pk):
-    try:
-        board = Boards.objects.get(pk=pk)
-        topics=board.topics.annotate(replies=Count('posts'))
-    except Boards.DoesNotExist:
-        raise Http404
-    return render(request, 'topics.html', {'board': board,'topics':topics})
+
+class TopicsListView(ListView):
+    model=Topics
+    template_name='topics.html'
+    context_object_name='topics'
+    paginate_by=10
+
+    def get_context_data(self,**kwargs):
+        kwargs['board']=self.board
+        return super().get_context_data(**kwargs)
+
+    def get_queryset(self):
+        self.board=get_object_or_404(Boards, pk=self.kwargs.get("pk"))
+        queryset=self.board.topics.order_by('-last_updated').annotate(replies=Count('posts')-1)
+        return queryset
 
 @login_required(login_url='login')
 def new_topic(request, pk):
@@ -44,11 +55,30 @@ def new_topic(request, pk):
     return render(request, 'new_topic.html', {'board': board, 'form': form})
 
 
-def topic_posts(request,pk,topic_pk):
-    topic=get_object_or_404(Topics,board__pk=pk, pk=topic_pk)
-    topic.views+=1
-    topic.save()
-    return render(request,'topic_posts.html',{'topic':topic})
+class PostListView(ListView):
+    model=Post
+    context_object_name='posts'
+    template_name='topic_posts.html'
+    paginate_by=2
+
+    def get_context_data(self,**kwargs):
+
+        session_key = 'viewed_topic_{}'.format(self.topic.pk)
+        # print(self.request.session[session_key])
+        if not self.request.session.get(session_key, False):
+            # print(self.request.session[session_key])
+            self.topic.views += 1
+            self.topic.save()
+            self.request.session[session_key] = True        
+        kwargs['topic']=self.topic
+        return super().get_context_data(**kwargs)
+
+    def get_queryset(self):
+        self.topic=get_object_or_404(Topics, board__pk=self.kwargs.get('pk'), pk=self.kwargs.get('topic_pk'))
+        queryset=self.topic.posts.order_by('created_at')
+        return queryset
+        
+    
 
 @login_required(login_url='login')
 def reply_topic(request,pk,topic_pk):
@@ -62,10 +92,28 @@ def reply_topic(request,pk,topic_pk):
             post.topic=topic
             post.created_by=request.user
             post.save()
+
+            topic.last_updated=timezone.now()
+            topic.save()
             return redirect('topic_posts',pk=pk,topic_pk=topic_pk)
         
     else:
         form=PostForm()
     return render(request,'reply_topic.html',{'topic':topic ,'form':form})
 
-    
+@method_decorator(login_required, name='dispatch')
+class PostUpdateView(UpdateView):
+    model=Post
+    fields=('message',)
+    pk_url_kwarg='post_pk'
+    template_name='edit_post.html'
+    context_object_name='post'
+
+    def form_valid(self,form):
+        post=form.save(commit=False)
+        post.updated_by=self.request.user
+        post.updated_at=timezone.now()
+        post.save()
+        return redirect('topic_posts',pk=post.topic.board.pk,topic_pk=post.topic.pk)
+
+
--- sush/urls.py
+++ sush/urls.py
@@ -24,13 +24,13 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
    
-    path('',views.home,name='home'),
+    path('',views.BoardsListView.as_view(),name='home'),
     re_path(r'^signup/$', accounts_views.signup, name='signup'),
     re_path(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     re_path(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
-    re_path(r'^boards/(?P<pk>\d+)/$',views.board_topics,name='board_topics'),
+    re_path(r'^boards/(?P<pk>\d+)/$',views.TopicsListView.as_view(),name='board_topics'),
     re_path(r'^boards/(?P<pk>\d+)/new/$',views.new_topic,name='new_topic'),
-    url(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/$', views.topic_posts, name='topic_posts'),
+    url(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/$', views.PostListView.as_view(), name='topic_posts'),
     re_path(r'^reset/$',auth_views.PasswordResetView.as_view(template_name='password_reset.html',
                                                             email_template_name='password_reset_email.html',
                                                             subject_template_name='password_reset.txt'),
@@ -50,6 +50,8 @@ urlpatterns = [
     url(r'^settings/password/done/$', auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'),name='password_change_done'),
 
     url(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/reply/$', views.reply_topic, name='reply_topic'),
+
+    url(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/posts/(?P<post_pk>\d+)/edit/$', views.PostUpdateView.as_view(template_name='edit_post.html'),name="edit_post"),
 
     path('admin/', admin.site.urls),
    
